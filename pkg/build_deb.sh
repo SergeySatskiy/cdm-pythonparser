9c5f628b6467785856ab54c0cff1b067b0e608b4
Sergey Fukanchik <fukanchik@gmail.com>
2016-07-21 22:23:59 -0400

    Rename build to cdm-pythonparser

--- pkg/build_deb.sh
+++ pkg/build_deb.sh
@@ -6,14 +6,28 @@ version="$3"
 
 cp ~/result/${projectfull}.tar.gz ${project}_${version}.orig.tar.gz
 
+# Use unstable for all debians
+# but must set distro name for ubuntu
+DISTRO=unstable
+VENDOR=$(lsb_release -si)
+
+if [ "${VENDOR}" == "Ubuntu" ]; then
+	DISTRO=$(lsb_release -sc)
+fi
+
 tar xzf ${project}_${version}.orig.tar.gz
 
 (
+	export DH_VERBOSE=1
+	export DEBIAN_FRONTEND="noninteractive"
+	export DEBFULLNAME="Sergey Satskiy"
+	export DEBEMAIL="sergey.satskiy@gmail.com"
+	export CDM_PROJECT_BUILD_VERSION="${version}"
 	cd ${projectfull}
 	sudo mk-build-deps -i --tool "apt-get -y"
 	rm -f *.deb
-	dch --force-bad-version --distribution unstable --package cdmpyparser --newversion ${version}-$(lsb_release -si)~$(lsb_release -sc) "new release"
-	debuild --set-envvar CDM_PYPARSER_BUILD_VERSION="${version}" -us -uc
+	dch --force-bad-version --distribution ${DISTRO} --package ${project} --newversion ${version}-$(lsb_release -si)~$(lsb_release -sc) "new release"
+	debuild --set-envvar CDM_PROJECT_BUILD_VERSION="${version}" -us -uc
 )
 
 #mv -f *.deb ~/result/
--- pkg/travis.sh
+++ pkg/travis.sh
@@ -10,19 +10,22 @@ fi
 
 VERSION=${VERSION#v}
 
-project=cdmpyparser
-projectfull=cdmpyparser-${VERSION}
-
-echo "Building ${projectfull}"
-
 TRAVIS_REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2)
 DOCKER_REPO=tarantool/build
 
+project=${TRAVIS_REPO_NAME}
+projectfull=${project}-${VERSION}
+
+echo "Building ${projectfull}"
+
 cd ..
+
+DOCKER_EXTRA=""
 
 case ${PACK} in
 	deb)
 		TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}-${DIST}
+		DOCKER_EXTRA="RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections"
 		;;
 	rpm)
 		TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}${DIST}
@@ -44,6 +47,7 @@ cp ${TRAVIS_REPO_NAME}/pkg/build_${PACK}.sh out/
 
 echo "FROM ${TARANTOOL_IMAGE}" > Dockerfile
 echo "RUN useradd -s ${SHELL} -u $(id -u) -d ${HOME} ${USER}" >> Dockerfile
+[ -z "${DOCKER_EXTRA}" ] || echo "${DOCKER_EXTRA}" >> Dockerfile
 echo "RUN usermod -a -G wheel ${USER} || :;\\" >> Dockerfile
 echo "    usermod -a -G adm ${USER} || :;\\" >> Dockerfile
 echo "    usermod -a -G sudo ${USER} || :;\\" >> Dockerfile
@@ -57,4 +61,7 @@ mv Dockerfile out/
 docker build --rm=true --quiet=true -t ${TARANTOOL_IMAGE}-for-${USER} out
 
 docker run --volume ${PWD}/out:/home/${USER}/result --workdir /home/${USER}/result --rm=true --user=${USER} ${TARANTOOL_IMAGE}-for-${USER} bash -x /home/${USER}/result/build_${PACK}.sh ${projectfull} ${project} ${VERSION}
+
+echo "Result of the build"
+ls out/
 
